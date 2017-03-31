835430e7e4303aab2e3fc6bc5e556fc8c182421f
MedSyl10 <mohamed.sylla.info@gmail.com>
2017-03-31 03:16:57 +0200

    cc

--- Publication/Interfaces/Bloc.h
+++ Publication/Interfaces/Bloc.h
@@ -10,18 +10,18 @@
 #define KEY_SIZE 4 // taille en byte des cle (publique, privee) a valider avec les groupes specifiant et developpant la signature
 #define HASH_SIZE 64  // taille du hash, a valider avec les groupes specifiant et developpant le hacheur
 
-class TXI
+class TXI // transaction input
 {
 	unsigned int nBloc;
-	unsigned int nTx;
-	unsigned int nUtxo;
+	unsigned int nTx;   // hash of the previous transaction
+	unsigned int nUtxo; // index of output
 	unsigned char signature[64];
 };
 
 class UTXO // une sortie non dépensée
 {
 	float montant;
-	unsigned char dest[KEY_SIZE];  //compte destinataire
+	unsigned char dest[KEY_SIZE];  //compte destinataire , public key
 	unsigned char hash[HASH_SIZE];    // hash(nBloc,nTx,nUTXO,montant,destinataire) pour securisation de l'UTXO
 };
 
@@ -36,9 +36,9 @@ class TXM { // transaction du mineur
 
 class Bloc
 {
-	char hash[64]; // hash des autres champs
+	char hash[64]; // hash des autres champs, hash of the entire transaction
 
-	char previous_hash[HASH_SIZE];
+	char previous_hash[HASH_SIZE]; // hash of the previous transaction 
 	unsigned int nonce;
 	int num; // numero du bloc, commence a zero
 	TX tx1; //  transaction du bloc
--- Publication/Interfaces/VerifierBloc.h
+++ Publication/Interfaces/VerifierBloc.h
@@ -2,7 +2,11 @@
 #define VerifierBloc_h
 
 namespace VerifierBloc {
-	
+	/*
+	 on doit s'assurer que la somme des sorties est moins que la somme des entrées 
+	 pour chaque transaction.
+	 la signature de la transaction doit etre correct (bien signée)
+	*/
 	bool verificationTransaction(TX transaction);
 	bool verificationTransaction(TXM transaction);
 	bool verificationBloc(Bloc bloc);
