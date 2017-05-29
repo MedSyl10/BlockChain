be68e45e844fb94a7738c362c60d5438e7dbc82d
MarLeo <martin.aurele12@gmail.com>
2017-05-29 10:15:27 +0200

    new hash

---
+++ Hacheur/Hacheur/Hasheur.h
@@ -0,0 +1,44 @@
+#pragma once
+
+
+#ifdef HACHEURDLL_EXPORTS
+#define HACHEURDLL_API __declspec(dllexport) 
+#else
+#define HASHEURDLL_API __declspec(dllimport) 
+#endif
+
+#include "Sha256.h"
+#include "Bloc.h"
+#include<sstream>
+
+namespace Hash 
+{
+
+	class Hash 
+	{
+	public:
+
+		/**
+		* Prend en entrée une chaine de caractères
+		* Retourne le hash de la chaine de caractères en entrée en utilisant l'algorithme SHA-256 (256 bits en sortie)
+		*/
+		static HASHEURDLL_API std::string hash(std::string chaine);
+
+		/**
+		* Prend en entrée une chaine de caractères et un hash (chaine de caractères)
+		* Vérifie que le hash de la chaine correspond bien au hash en entrée
+		*/
+		static HASHEURDLL_API bool checkHash(std::string chaine, std::string verify);
+
+		/**
+		* Prend en entrée un bloc
+		* Retourne une chaine de caractères contenant les données du bloc dans un format quelconque
+		*/
+		static HASHEURDLL_API std::string blocToString(Bloc bloc);
+
+		static HASHEURDLL_API std::string charToString(unsigned char* tab, unsigned int size);
+		static HASHEURDLL_API std::string charToString(char tab[], unsigned int size);
+
+	};
+
+}
